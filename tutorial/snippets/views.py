aaa488da0f897be783b25ff83d58e6f084b8cb17
FrankApiyo <franklineapiyo@gmail.com>
2019-10-26 16:02:00 +0300

    Fix typo

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -43,7 +43,7 @@ class SnippetDetail(APIView):
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Resonse(serializer.data)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
